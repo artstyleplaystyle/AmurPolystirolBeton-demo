c4f42346e305cd6ac06d9e716b62786dc19f83d9
ArtStyle <artiknem4@gmail.com>
2025-02-24 18:16:43 +1000

    Larger Work time

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -51,14 +51,14 @@ const Contacts: React.FC = () => {
         </div>
       </div>
       <motion.div
-          className="flex justify-between gap-2 bg-gray-100 p-4 rounded-lg shadow-md"
+          className="flex justify-between gap-2 bg-gray-100 p-4 rounded-xl shadow-md"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
           >
       {/* Виджет дней недели */}
       <div className="mt-8 text-xl max-w-md mx-auto">
-        <h3 className="text-lg font-semibold text-gray-800 mb-4 text-center">
+        <h3 className="text-3xl font-semibold text-gray-800 mb-4 text-center">
           Режим работы
         </h3>
         <div className="flex justify-between gap-2 bg-gray-100 p-4 rounded-lg shadow-md">
